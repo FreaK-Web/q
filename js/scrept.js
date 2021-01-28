4a318ca07f657d33de9c169e08d8a97fc8e7d6c1
Ivan <eample@gmail.com>
2021-01-28 21:42:00 +0300

    add 3x

--- js/scrept.js
+++ js/scrept.js
@@ -1,6 +1,8 @@
 
 const a = prompt('Как твои делая?', "");
 const b = prompt('Как твои делая?', "");
+const c = prompt('Как твои делая?', "");
+ console.log(c);
  console.log(a);
  console.log(b);
 
